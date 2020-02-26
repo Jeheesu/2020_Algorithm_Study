146ece12553ea4f7b4a9ebf4aeae5af8da5c6c76
Jeheesu <heesu0308@naver.com>
2020-02-26 23:45:31 +0900

    dfsx

---
+++ 과제제출폴더/5주차/희수/Node.java
@@ -0,0 +1,12 @@
+package week5;
+
+public class Node {
+	public String area;
+	public double lo;
+	public double la;
+	public double w;
+	public int v1 = 0;
+	public int v2 = 0;
+	public int num;
+	public Node next;	
+}
